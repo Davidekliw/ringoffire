97d4893f34f274060a2005da03d329dc2558f799
David Wilke <david-wilke@live.com>
2025-06-09 21:14:36 +0200

    add logic to add players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,34 +1,62 @@
-import { Component, OnInit, ɵgenerateStandaloneInDeclarationsError } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
+import { addToDebugVariable, ownDebugLogger } from '../../assets/debug';
+import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from "../game-info/game-info.component";
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule, MatFormFieldModule, GameInfoComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  currentCard: string | undefined = '';
+  currentCard: string | undefined = 'card_cover';
   game!: Game;
+  flyCard = false;
+  // name: string = 'Max Mustermann';
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.game = new Game();
   }
 
   ngOnInit(): void {
-    this.newGame;
+    this.newGame();
+    addToDebugVariable('game', this.game);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+      console.log('The dialog was closed', name);
+      // this.name = result;
+    });
   }
 
   newGame() {
     this.game = new Game();
-    console.log(this.game);
+    ownDebugLogger(`game`, this.game);
   }
 
   takeCard() {
     this.currentCard = this.game.stack.pop();
-    this.game.playedCards.push(this.currentCard!);
+    this.flyCard = true;
+    setTimeout(() => {
+      this.flyCard = false;
+      this.game.playedCards.push(this.currentCard!);
+      ownDebugLogger(`this.currentCard`, this.currentCard);
+    }, 1000);
   }
 }

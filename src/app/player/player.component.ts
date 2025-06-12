26dc7026c766cf5962c6b85438b2940ae359d9e3
David Wilke <david-wilke@live.com>
2025-06-12 19:47:07 +0200

    remove unused imports

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -3,10 +3,6 @@ import { Component, Input, input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { GameComponent } from "../game/game.component";
-import { Game } from '../../models/game';
-import { addToDebugVariable, ownDebugLogger } from '../../assets/debug';
-import { Title } from 'chart.js';
 
 @Component({
   selector: 'app-player',
